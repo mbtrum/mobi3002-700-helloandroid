050e6ff3408c8378d1b1ac5be226d146c7d0067b
Michael Trumbull <michael.trumbull@nscc.ca>
2025-09-15 16:18:30 -0300

    Added some Compose elements.

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.helloandroid.ui.theme.HelloAndroidTheme
 
 // Main Activity
@@ -20,29 +27,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HelloAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Starting point for the app
+                Home()
+                //Hello()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home() {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Hello"
+        )
+        Text(
+            text = "World"
+        )
+        Text(
+            text = "Hello"
+        )
+        Text(
+            text = "World"
+        )
+    }
+
+//    Row(
+//        horizontalArrangement = Arrangement.Center,
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.fillMaxSize()
+//    ){
+//
+//    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    HelloAndroidTheme {
-        Greeting("Android")
-    }
+fun Hello() {
+    Text(
+        text = "Hello",
+        color = Color.Red,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(48.dp)
+    )
 }
---
+++ app/src/main/java/com/example/helloandroid/ui/screens/Home.kt
@@ -0,0 +1,1 @@
+package com.example.helloandroid.ui.screens
